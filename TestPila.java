3661866bd8da6d44bc63f65a4e0768402406085a
NicolasCaporale <nicolas.caporale@marconibari.edu.it>
2026-04-28 07:15:21 +0200

    Update TestPila.java

--- TestPila.java
+++ TestPila.java
@@ -1,1 +1,43 @@
-//Implementare qui la classe TestPila, per l'esercizio delle parentesi bilanciate
+import java.util.Stack;
+
+public class TestPila {
+
+    public static boolean parentesiBilanciate(String espressione) {
+        Stack<Character> pila = new Stack<>();
+
+        for (int i = 0; i < espressione.length(); i++) {
+            char c = espressione.charAt(i);
+
+            if (c == '(' || c == '[' || c == '{') {
+                pila.push(c);
+            } else if (c == ')' || c == ']' || c == '}') {
+                if (pila.isEmpty()) return false;
+
+                char aperta = pila.pop();
+
+                if ((c == ')' && aperta != '(') ||
+                    (c == ']' && aperta != '[') ||
+                    (c == '}' && aperta != '{')) {
+                    return false;
+                }
+            }
+        }
+
+        return pila.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String[] test = {
+            "{[()]}",
+            "{[(])}",
+            "((()))",
+            "(]",
+            "{[}",
+            ""
+        };
+
+        for (String s : test) {
+            System.out.println(s + " -> " + (parentesiBilanciate(s) ? "bilanciata" : "sbilanciata"));
+        }
+    }
+}
