36af7afb6dad0f0afa51f4ac29c43a658ba66fe0
NicolasCaporale <nicolas.caporale@marconibari.edu.it>
2026-04-28 07:11:44 +0200

    Update Pila.java

--- Pila.java
+++ Pila.java
@@ -1,1 +1,62 @@
-//Implementare qui la classe Pila
+public class Pila<T> {
+    private Nodo<T> top;
+
+    public Pila() {
+        this.top = null;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void push(T dato) {
+        Nodo<T> nuovoNodo = new Nodo<>(dato);
+        nuovoNodo.next = top;
+        top = nuovoNodo;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Pila vuota");
+        }
+
+        T dato = top.dato;
+        top = top.next;
+        return dato;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Pila vuota");
+        }
+
+        return top.dato;
+    }
+
+    public int size() {
+        int contatore = 0;
+        Nodo<T> corrente = top;
+
+        while (corrente != null) {
+            contatore++;
+            corrente = corrente.next;
+        }
+
+        return contatore;
+    }
+
+    @Override
+    public String toString() {
+        String str= "[ "
+
+        Nodo<T> corrente = top;
+        while (corrente != null) {
+            str += corrente.dato;
+            str += " → ";
+            corrente = corrente.next;
+        }
+
+        str += "]"
+        return str;
+    }
+}
